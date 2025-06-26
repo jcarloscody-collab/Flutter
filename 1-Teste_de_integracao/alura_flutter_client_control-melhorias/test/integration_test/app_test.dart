a6b60973d75ed281be2d34aaac242bad288b25c3
Josue Carlos <josuecarlosos2@gmail.com>
2025-06-26 00:33:56 -0300

    test: teste de integração - alura

--- 1-Teste_de_integracao/alura_flutter_client_control-melhorias/test/integration_test/app_test.dart
+++ 1-Teste_de_integracao/alura_flutter_client_control-melhorias/test/integration_test/app_test.dart
@@ -14,10 +14,45 @@ void main(){
 
     await widgetTester.pumpAndSettle(); //garantir que tudo foi inicializado
 
-    expect(find.text('Menu'), findsNothing);
+    expect(find.text('Clientes'), findsOneWidget);
+    expect(find.byIcon(Icons.menu), findsOneWidget);
+    expect(find.byType(FloatingActionButton), findsOneWidget);
+
     await widgetTester.tap(find.byIcon(Icons.menu));
     await widgetTester.pumpAndSettle();
+
     expect(find.text('Menu'), findsOneWidget);
+    expect(find.text('Gerenciar clientes'), findsOneWidget);
+    expect(find.text('Tipos de clientes'), findsOneWidget);
+    expect(find.text('Sair'), findsOneWidget);
+
+    await widgetTester.tap(find.text('Tipos de clientes'));
+    await widgetTester.pumpAndSettle();
+
+    expect(find.text('Tipos de clientes'), findsOneWidget);
+    expect(find.byType(FloatingActionButton), findsOneWidget);
+    expect(find.byIcon(Icons.menu), findsOneWidget);
+    expect(find.text('Platinum'), findsOneWidget);
+    expect(find.text('Golden'), findsOneWidget);
+    expect(find.text('Titanium'), findsOneWidget);
+    expect(find.text('Diamond'), findsOneWidget);
+
+    await widgetTester.tap(find.byType(FloatingActionButton));
+    await widgetTester.pumpAndSettle();
+    expect(find.byType(AlertDialog), findsOneWidget);
+    await widgetTester.enterText(find.byType(TextFormField), 'Ferro');
+
+    await widgetTester.tap(find.text('Selecionar icone'));
+    await widgetTester.pumpAndSettle();
+
+    await widgetTester.tap(find.byIcon(Icons.card_giftcard));
+    await widgetTester.pumpAndSettle();
+
+    await widgetTester.tap(find.text('Salvar'));
+    await widgetTester.pumpAndSettle();
+    expect(find.text('Ferro'), findsOneWidget);
+    expect(find.byIcon(Icons.card_giftcard), findsOneWidget);
+
   },);
 
 }
